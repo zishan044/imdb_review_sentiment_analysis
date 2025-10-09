4f10fdb7815d279710c2c9dd2441e286dbb0cbe9
zishan044 <winchesterfelix007@gmail.com>
2025-10-09 18:20:43 +0600

    added stopword removal in data processing pipeline

--- src/imdb_review_sentiment_analysis/pipelines/data_processing/nodes.py
+++ src/imdb_review_sentiment_analysis/pipelines/data_processing/nodes.py
@@ -1,17 +1,22 @@
 import pandas as pd
 import nltk
-from nltk import sent_tokenize, word_tokenize, pos_tag
+from multiprocessing import Pool, cpu_count
+from nltk import word_tokenize, pos_tag
 from nltk.stem import WordNetLemmatizer
-from nltk.corpus import wordnet
+from nltk.corpus import wordnet, stopwords
 
 nltk.download('punkt', quiet=True)
 nltk.download('averaged_perceptron_tagger_eng', quiet=True)
 nltk.download('wordnet', quiet=True)
+nltk.download('stopwords', quiet=True)
 
 lemmatizer = WordNetLemmatizer()
+stop_words = set(stopwords.words('english')) - {'no', 'not', 'nor', "n't"}
+
+def remove_stopwords(text):
+    return ' '.join([w for w in text.split(' ') if w not in stop_words])
 
 def get_wordnet_pos(tag: str) -> str:
-
     if tag.startswith('J'):
         return wordnet.ADJ
     elif tag.startswith('V'):
@@ -24,7 +29,6 @@ def get_wordnet_pos(tag: str) -> str:
         return wordnet.NOUN
 
 def clean_text(x: pd.Series) -> pd.Series:
-
     return (
         x.str.lower()
          .str.replace(r'<.*?>', ' ', regex=True)
@@ -35,20 +39,20 @@ def clean_text(x: pd.Series) -> pd.Series:
     )
 
 def lemmatize_text(text: str) -> str:
-
     tokens = word_tokenize(text)
     pos_tags = pos_tag(tokens)
     lemmas = [lemmatizer.lemmatize(w, get_wordnet_pos(t)) for w, t in pos_tags]
     return " ".join(lemmas)
 
 def preprocess_data(df: pd.DataFrame) -> pd.DataFrame:
-    
     df_process = df.copy()
 
     df_process['review'] = clean_text(df_process['review'])
     df_process['lemmatized_text'] = df_process['review'].apply(lemmatize_text)
+    
+    with Pool(cpu_count()) as p:
+        df_process['processed_text'] = p.map(remove_stopwords, df_process['lemmatized_text'])
+
     df_process['pos_senti'] = (df_process['sentiment'] == 'positive').astype(int)
 
-    df_process = df_process[['lemmatized_text', 'pos_senti']]
-
-    return df_process
+    return df_process[['processed_text', 'pos_senti']]
